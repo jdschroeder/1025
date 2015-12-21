d9c702b8ad15343773a9b90bdd527ffc4dde0ed0
Josh Schroeder <jschroeder@yukoncollege.yk.ca>
2015-12-21 14:49:15 -0800

    Basic javascript scorecard functionality

---
+++ javascripts/1025score.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function() {
+    function sumTotalScore() {
+        var totalScore = 0;
+        $(".rowscore").each(function() { totalScore += parseInt($(this).html()); });
+        $("#totalscore").html(totalScore);
+    }
+    function calculateRowScore(row) {
+        var rowScore = 0;
+        var checkedBoxes = $("input[data-distance=" + row + "]:checked");
+        if(checkedBoxes.length == 6) { rowScore += parseInt(row); } // Calculate bonus for scoring all six at a distance
+        $(checkedBoxes).each(function(box) { rowScore += parseInt($(this).attr("data-points")); });
+        $(".rowscore[data-distance=" + row + "]").html(rowScore);
+    }
+    $("input[type=checkbox]").on("change", function() {
+        calculateRowScore($(this).attr('data-distance')); // Calculate scores a row at a time for efficiency
+        sumTotalScore(); // Re-sum total score after a row has changed
+    });
+}, $);
